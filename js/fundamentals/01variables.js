cae6c97ccc44c5782cd1261c4813a573de82d660
Ayush <ayusharma0xyz@gmail.com>
2026-09-07 15:02:36 +0530

    Adding the first employee !

---
+++ js/fundamentals/01variables.js
@@ -0,0 +1,9 @@
+const employeeName = "Rahul";
+const employeeAge = 25;
+const monthlySalary = 50000;
+const isActive = true;
+
+console.log(employeeName);
+console.log(employeeAge);
+console.log(monthlySalary);
+console.log(isActive);
